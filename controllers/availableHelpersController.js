a85a7e53bf99d4c6fd29a7ca5860ca3afc290e34
Dhyey89 <dhyey892@gmail.com>
2023-04-12 12:43:02 -0400

    availablehelpers commit

--- controllers/availableHelpersController.js
+++ controllers/availableHelpersController.js
@@ -6,7 +6,6 @@ const Helper = require("../models/adminModel");
 const Booking = require("../models/bookingModel");
 
 exports.getAvailableTimeSlots = async (req, res, next) => {
-  const day = req.query.day;
   const date = req.query.date;
   const location = req.query.location;
   const timeSlots = ["9:00 am", "9:30 am", "10:00 am", "10:30 am", "11:00 am", "11:30 am", "12:00 pm", "12:30 pm", "1:00 pm", "1:30 pm", "2:00 pm", "2:30 pm", "3:00 pm", "3:30 pm", "4:00 pm", "4:30 pm"];
