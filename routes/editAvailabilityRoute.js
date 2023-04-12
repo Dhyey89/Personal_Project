88cc90fe304df79fe7b1808ee53e6123e7849c33
Dhyey89 <dhyey892@gmail.com>
2023-04-12 14:41:54 -0400

    deleted

--- routes/editAvailabilityRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const editAvailabilityController = require('../controllers/editAvailabilityController');
-const router = express.Router();
-
-router.put('/editAvailability/:helperId', editAvailabilityController.updateAvailability);
-router.get('/getAvailability/:helperId', editAvailabilityController.getAvailability);
-
-module.exports = router;
