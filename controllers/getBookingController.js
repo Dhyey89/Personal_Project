12256cac7f5c2f0115e306ad029bf6e6b8a32763
Dhyey89 <dhyey892@gmail.com>
2023-04-13 11:26:45 -0400

    delete unnecessary

--- controllers/getBookingController.js
+++ controllers/getBookingController.js
@@ -1,16 +1,5 @@
 const Booking = require("../models/bookingModel");
-exports.getPendingRequests = async (req, res, next) => {
-    const helperId = req.helperId;
-  
-    try {
-      const bookings = await Booking.find({ helper: helperId, status: 'pending' }).populate('user', 'firstname lastname');
 
-      res.send(bookings);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Server Error');
-    }
-  };
   exports.getCurrentBooking = async (req, res, next) => {
     const userId = req.userId;
   
@@ -24,24 +13,10 @@ exports.getPendingRequests = async (req, res, next) => {
     
   };
   exports.getConfirmedBooking = async (req, res, next) => {
-    const helperId = req.helperId;
-  
     try {
-      const bookings = await Booking.find({ helper: helperId,status: { $in: ['accepted']}}).populate('user', 'firstname lastname');
-
+      const bookings = await Booking.find().populate('user', 'firstname lastname');
+  
       res.send(bookings);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Server Error');
-    }
-  };
-  exports.getUserBookingHistory = async (req, res, next) => {
-    const userId = req.userId;
-  
-    try {
-      const bookings = await Booking.find({ user: userId,status: { $in: ['pending','completed','rejected']}}).populate('helper', 'firstname lastname');
-
-      return res.send(bookings);
     } catch (err) {
       console.error(err);
       res.status(500).send('Server Error');
--- models/adminModel.js
+++ models/adminModel.js
@@ -14,87 +14,14 @@ const adminSchema = new Schema(
         type:String,
        
     },
-    dob: {
-      type: String,
-      default:'' 
-    },
     email: {
       type: String,
       required: true
     },
-    mob: {
-      type: String,
-      default:''
-    },
-    gender: {
-      type: String,
-      default: ''
-    },
-    age: {
-      type: Number,
-      default: 0
-    },
-    nationality:{
-      type:String,
-    },
-    helped: {
-      type: Number,
-      default: 0
-    },
-    profilePhotoUrl:{
-      type:String
-    },
-    email_verified:{
-        type:Boolean,
-        default:false
-    },
-    rating:{
-      type:Number
-    },
-    feedback:{
-      type:String
-    },
-    helperId:{
+ perId:{
         type:Schema.Types.ObjectId,
         ref:'admin'
     },
-    description:{
-      type: String,
-      default: ''
-    },
-    profilePhotoUrl:{
-      type:String
-    },
-   availability: {
-    Monday: {
-      type: String,
-      default: ""
-    },
-    Tuesday: {
-      type: String,
-      default: ""
-    },
-    Wednesday: {
-      type: String,
-      default: ""
-    },
-    Thursday: {
-      type: String,
-      default: ""
-    },
-    Friday: {
-      type: String,
-      default: ""
-    },
-    Saturday: {
-      type: String,
-      default: ""
-    },
-    Sunday: {
-      type: String,
-      default: ""
-    }
-  },
   bookings: [{
     type: Schema.Types.ObjectId,
     ref: 'Booking'
