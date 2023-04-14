b9750c4b284358c41fc1e0e452cb63640eedb2de
Dhyey89 <dhyey892@gmail.com>
2023-04-13 22:11:47 -0400

    mail changes

--- controllers/deleteBookingController.js
+++ controllers/deleteBookingController.js
@@ -38,7 +38,7 @@ exports.deleteBooking = async (req, res, next) => {
 
 <p>We are sorry to hear that you have cancelled your booking with our helper. We understand that circumstances can change, and we appreciate you letting us know in advance.</p> 
 <p>If you would like to rebook our services in the future, please don't hesitate to contact us. We value your business and would be more than happy to assist you with any of your care needs.</p> 
-<p>Thank you for choosing CommuteCare. If you have any questions or concerns, please do not hesitate to contact us by phone or email.</p>
+<p>Thank you for choosing Assisttrack. If you have any questions or concerns, please do not hesitate to contact us by phone or email.</p>
       
 <p>Best regards,</p>
 Assisttrack Team,
